ee9544f923e06d8adbdafa8b3a1e8d5492546aaf
Max Smagin <Max@MacBook-Air-MS.local>
2021-11-17 17:09:03 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [timer, setTimer] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+    } else if (!isActive && timer !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, timer]);
+
+
+   function reset() {
+     setTimer(0);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{timer}</h1>
+      <button onClick={toggle}>Start</button>
+      <button onClick={toggle}>Stop</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
